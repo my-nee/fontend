9e25d09d0c89b3e986724fe412b44ad841a53492
my-nee <ltramy204@gmail.com>
2025-03-19 00:34:49 +0700

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,22 @@
 
-import PromoSections from "@/components/Promo_Sections";
-import CategoryPreviews from "@/components/Category_Previews";
-import ProductLists from "@/components/Product_Lists";
-import ProductFeatures from "@/components/Product_Features";
-
-export default function MainPage() {
-    return (
-        <>
-            <PromoSections />
-            <CategoryPreviews />
-            <ProductLists />
-            <ProductFeatures />
-        </>
-    );
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <nav >
+      <ul className="space-y-5">
+        <li>
+          <Link href={"/checkout"}>
+         Checkout
+          </Link>
+        </li>
+        <li style={{ margin: "20px 0" }}>
+          <Link href={"/login"}> Login</Link>
+        </li>
+        <li>
+          <Link href={"/dashboard"}> Dashboard</Link>
+        </li>
+          <div> Để em xem thiết kế như nào đã</div>
+      </ul>
+    </nav>
+  );
 }
